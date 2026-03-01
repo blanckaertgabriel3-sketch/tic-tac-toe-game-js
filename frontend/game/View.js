f18470b514a3a21716c3c09ff2f5c0c4f9b35f4a
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-03-01 16:19:52 +0100

    adjust game size

--- frontend/game/View.js
+++ frontend/game/View.js
@@ -1,13 +1,25 @@
 export default class View {
 	constructor() {
 		this.game = document.getElementById("game");
-		this.game.style.width = "500px";
-		this.game.style.height = "500px";
+		this.game.style.width = "480px";
+		this.game.style.height = "480px";
 		this.game.style.backgroundColor = "#992208";
 
-		for(let i=0 ; i<9 ; i++) {
+		this.columnsNb = 3;
+		this.rowsNb = 3;
+
+		this.game.style.display = "grid";
+		this.game.style.border = "10px solid black";
+		this.game.style.gridTemplateColumns = `repeat(${this.columnsNb}, 1fr)`;
+		this.game.style.gridTemplateRows = `repeat(${this.rowsNb}, 1fr)`;
+		
+	}
+	createButton() {
+		for(let i=0 ; i<this.columnsNb*this.rowsNb ; i++) {
 			var x = document.createElement("button");
-			game.appendChild(x);
+			x.style.border = "2px solid black";
+			this.game.appendChild(x);
+
 			
 		}
 	}
