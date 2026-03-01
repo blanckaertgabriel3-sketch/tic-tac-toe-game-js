67b584b39598fe2b657f103b31269e9da04024f9
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-03-01 15:32:25 +0100

    correct create Element

--- frontend/game/View.js
+++ frontend/game/View.js
@@ -5,13 +5,6 @@ export default class View {
 		this.game.style.height = "500px";
 		this.game.style.backgroundColor = "#992208";
 
-		const button = document.createElement("div");
-		button.style.height = "50px";
-		button.style.width = "50px";
-		button.style.backgroundColor = "green";
-		console.log("button create");
-		console.log(button);
-
 		for(let i=0 ; i<9 ; i++) {
 			var x = document.createElement("button");
 			game.appendChild(x);
